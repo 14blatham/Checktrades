41f29eaa31d94420e1f794cfc6e6ca1c4cbe69af
Oliver Latham <166565145+14blatham@users.noreply.github.com>
2026-04-20 22:35:53 +0100

    Add menu toggle functionality on click

--- page-transitions.js
+++ page-transitions.js
@@ -17,5 +17,8 @@
     e.preventDefault();
     document.body.classList.add('page-leaving');
     setTimeout(function () { window.location.href = href; }, 200);
+    document.querySelector('[data-icon="menu"]')?.addEventListener('click', () => {
+  document.querySelector('nav ul')?.classList.toggle('hidden');
+});
   });
 })();
